7cbb4cdf49035e9de70ea03af542b5e1b7571ef2
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-01-06 05:42:09 +0200

    fix exact hours bug, add stylings

--- server/app.js
+++ server/app.js
@@ -2,27 +2,24 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = 3000;
+
 app.use(cors());
 app.use(express.json());
 
 app.post('/convertTime', (req, res) => {
     const { time } = req.body;
-    const hours = time.slice(0,2);
-    const minutes = time.slice(3,5);
-    console.log(`${hours} and ${minutes}`)
-
+    const hours = parseInt(time.slice(0, 2), 10);
+    const minutes = parseInt(time.slice(3, 5), 10);
+    console.log(`${hours} and ${minutes}`);
     function convertToAMPM(hours, minutes) {
         const ampm = hours >= 12 ? 'PM' : 'AM';
-        hours = hours % 12;
-        hours = hours ? hours : 12; 
-        minutes = minutes < 10 ? '0'+minutes : minutes;
-        const strTime = hours + ':' + minutes + ' ' + ampm;
-        return strTime;
+        const convertedHours = hours % 12 || 12; 
+        const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+        return `${convertedHours}:${formattedMinutes} ${ampm}`;
     }
-    res.json(convertToAMPM(hours, minutes))
-})
-
+    res.send(convertToAMPM(hours, minutes)); 
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
 
@@ -8,14 +7,18 @@ function App() {
     axios.post('http://localhost:3000/convertTime', {time})
     .then((res)=>{
       console.log(res.data)
+      document.querySelector('#convertedTime').innerHTML = res.data
     })
   }
   return (
-    <>
+    <div className='wrap'>
     <h3>Enter time:</h3>
-    <input type="time" />
-    <button onClick={convertTime}>Convert time</button>
-    </>
+    <div className="timeConvertion">
+      <input type="time" class="custom-time-input" />
+      <button onClick={convertTime} className='convertBtn'>Convert time</button>
+    </div>
+    <h3>Converted time: <div id="convertedTime"></div></h3>
+    </div>
   )
 }
 
